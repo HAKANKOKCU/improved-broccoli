4e6664381faf72e570a544e20a44712663ef0a12
Hakan <103432992+HAKANKOKCU@users.noreply.github.com>
2024-07-22 14:07:43 +0300

    File breaker source code (wont be updated anymore)

---
+++ FileBreaker/FileBreaker/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BackgroundWorker2 = New System.ComponentModel.BackgroundWorker()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button1.Location = New System.Drawing.Point(5, 8)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(128, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Select Files To Corrupt"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Multiselect = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CheckBox1.Location = New System.Drawing.Point(12, 37)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(164, 17)
+        Me.CheckBox1.TabIndex = 1
+        Me.CheckBox1.Text = "Create New Files For Corrupts"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button2.Location = New System.Drawing.Point(5, 213)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(275, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Corrupte!11"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(5, 184)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(275, 23)
+        Me.ProgressBar1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 136)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 4
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CheckBox2.Location = New System.Drawing.Point(12, 60)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(265, 17)
+        Me.CheckBox2.TabIndex = 5
+        Me.CheckBox2.Text = "Replace File With Empty String If Failled To Corrupt"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Checked = True
+        Me.CheckBox3.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.CheckBox3.Location = New System.Drawing.Point(12, 83)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(84, 17)
+        Me.CheckBox3.TabIndex = 6
+        Me.CheckBox3.Text = "Ingore Errors"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(116, 106)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {1000, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(81, 20)
+        Me.NumericUpDown1.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Break Time (In MS)"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Button3
+        '
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Button3.Location = New System.Drawing.Point(144, 8)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(136, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Select Folder To Corrupt"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label3.Location = New System.Drawing.Point(9, 149)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(268, 32)
+        Me.Label3.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(285, 245)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.ImeMode = System.Windows.Forms.ImeMode.[On]
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "FileBreaker"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents FolderBrowserDialog1 As System.Windows.Forms.FolderBrowserDialog
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents BackgroundWorker2 As System.ComponentModel.BackgroundWorker
+
+End Class
---
+++ FileBreaker/FileBreaker/Form1.vb
@@ -0,0 +1,135 @@
+﻿Public Class Form1
+    Dim randomer As New Random
+    Dim fps As Integer = 0
+    Dim cnt As Integer = 0
+    Dim filecount As Integer = 0
+    Dim ffoption As Boolean = True
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Button2.Enabled = False
+        CheckBox1.Enabled = False
+        CheckBox2.Enabled = False
+        CheckBox3.Enabled = False
+        NumericUpDown1.Enabled = False
+        Label1.Text = "Computing.."
+        Label1.Update()
+        filecount = IIf(ffoption = True, OpenFileDialog1.FileNames.Length, countFiles(FolderBrowserDialog1.SelectedPath).Count)
+        BackgroundWorker1.RunWorkerAsync({CheckBox1.Checked, CheckBox2.Checked, IIf(ffoption = "true", OpenFileDialog1.FileNames, countFiles(FolderBrowserDialog1.SelectedPath)), CheckBox3.Checked, NumericUpDown1.Value})
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim i = 0
+        Dim cnt As Integer
+        If TypeOf e.Argument(2) Is Array Then
+            cnt = e.Argument(2).length
+        Else
+            cnt = e.Argument(2).count
+        End If
+        For Each a As String In e.Argument(2)
+            Try
+                Dim bytz As Array = My.Computer.FileSystem.ReadAllBytes(a)
+                randomer.NextBytes(bytz)
+                If e.Argument(0) = True Then
+                    My.Computer.FileSystem.WriteAllBytes(a + ".corrupt", bytz, False)
+                Else
+                    My.Computer.FileSystem.WriteAllBytes(a, bytz, False)
+                End If
+
+            Catch ex As Exception
+                If e.Argument(1) = True Then
+                    Try
+                        My.Computer.FileSystem.WriteAllText(a, "", False)
+                    Catch exa As Exception
+                        If e.Argument(3) = False Then
+                            MsgBox(exa.Message)
+                        End If
+                    End Try
+                Else
+                    If e.Argument(3) = False Then
+                        MsgBox(ex.Message)
+                    End If
+                End If
+            End Try
+            i += 1
+            Dim crr As String = ""
+            Try
+                crr = e.Argument(2)(i)
+            Catch ex As Exception
+
+            End Try
+            BackgroundWorker1.ReportProgress((i / (cnt)) * 100, {i, crr})
+            System.Threading.Thread.Sleep(e.Argument(4))
+        Next
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(ByVal sender As System.Object, ByVal e As System.ComponentModel.ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+        cnt += 1
+        Label1.Text = e.ProgressPercentage.ToString + "% " + e.UserState(0).ToString + " of " + filecount.ToString + " files (" + fps.ToString + " Fips)"
+        Label3.Text = e.UserState(1)
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(ByVal sender As System.Object, ByVal e As System.ComponentModel.RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        Button2.Enabled = True
+        CheckBox1.Enabled = True
+        CheckBox2.Enabled = True
+        CheckBox3.Enabled = True
+        NumericUpDown1.Enabled = True
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(ByVal sender As System.Object, ByVal e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        Label1.Text = OpenFileDialog1.FileNames.Length.ToString + " files"
+        ffoption = True
+        Button1.BackColor = Color.Orange
+        Button3.BackColor = SystemColors.Control
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked Then
+            CheckBox2.Enabled = False
+            CheckBox2.Checked = False
+        Else
+            CheckBox2.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        fps = cnt
+        cnt = 0
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        If FolderBrowserDialog1.ShowDialog() = DialogResult.OK Then
+            ffoption = False
+            Button3.BackColor = Color.Orange
+            Button1.BackColor = SystemColors.Control
+        End If
+    End Sub
+    Private Function countFiles(ByVal driveRootPath As String) As ArrayList
+        Dim queue As New List(Of IO.DirectoryInfo)
+        Dim di As New IO.DirectoryInfo(driveRootPath)
+        queue.Add(di)
+        Dim fs As New ArrayList
+        Do
+            di = queue(0)
+            Try
+                queue.AddRange(di.GetDirectories)
+            Catch ex As Exception
+            End Try
+            Try
+                For Each aa As IO.FileInfo In di.GetFiles
+                    Try
+                        fs.Add(aa.FullName)
+                    Catch
+                    End Try
+                Next
+            Catch ex As Exception
+            End Try
+            queue.Remove(di)
+        Loop Until queue.Count = 0
+        Return fs
+    End Function
+End Class
